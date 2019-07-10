1a9132e191a43d12440c98290381871af139aba6
缐英达 <xianyingda@guazi.com>
2019-07-10 16:24:22 +0800

    21d+Dwq

--- src/main/java/cn/aegisa/sms/center/schedule/StatusChecker.java
+++ src/main/java/cn/aegisa/sms/center/schedule/StatusChecker.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Component;
  * @serial
  * @since 2019-03-14 12:45
  */
+@SuppressWarnings("Duplicates")
 @Component
 @Slf4j
 public class StatusChecker {
@@ -22,20 +23,33 @@ public class StatusChecker {
     @Autowired
     private DingPusher pusher;
 
-    private int count = 10;
 
-    @Scheduled(cron = "0/10 0/1 * * * ?")
+    @Scheduled(cron = "0/15 0/1 * * * ?")
     public void webChecker() {
         HttpRequest get = HttpUtil.createGet("http://zk.hagaozhong.com/");
-        get.timeout(5000);
+        get.timeout(10000);
         try {
             HttpResponse execute = get.execute();
             int status = execute.getStatus();
-            if (status < 400) {
-                if (count > 0) {
-                    pusher.pushTextMessage(new DingTextMessage("网站好像是可以访问啦！！！"));
-                }
-                this.count--;
+            if (status < 300) {
+                pusher.pushTextMessage(new DingTextMessage("http://zk.hagaozhong.com/网站好像是可以访问啦！！！"));
+            } else {
+                log.info("状态码：" + status);
+            }
+        } catch (Exception e) {
+            log.info("连接超时");
+        }
+    }
+
+    @Scheduled(cron = "0/15 0/1 * * * ?")
+    public void web1Checker() {
+        HttpRequest get = HttpUtil.createGet("http://zk.haedu.gov.cn/");
+        get.timeout(10000);
+        try {
+            HttpResponse execute = get.execute();
+            int status = execute.getStatus();
+            if (status < 300) {
+                pusher.pushTextMessage(new DingTextMessage("http://zk.haedu.gov.cn/网站好像是可以访问啦！！！"));
             } else {
                 log.info("状态码：" + status);
             }
