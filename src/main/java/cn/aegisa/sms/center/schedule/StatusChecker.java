abb602e4ccafe8d8a127551e9e1cdee681d3db03
缐英达 <xianyingda@guazi.com>
2019-07-10 11:32:20 +0800

    检查网站

--- src/main/java/cn/aegisa/sms/center/schedule/StatusChecker.java
+++ src/main/java/cn/aegisa/sms/center/schedule/StatusChecker.java
@@ -1,8 +1,13 @@
 package cn.aegisa.sms.center.schedule;
 
 import cn.aegisa.sms.center.push.DingPusher;
+import cn.aegisa.sms.center.push.DingTextMessage;
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
+import cn.hutool.http.HttpUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 /**
@@ -16,5 +21,18 @@ public class StatusChecker {
 
     @Autowired
     private DingPusher pusher;
+
+    @Scheduled(cron = "0/20 0/1 * * * ?")
+    public void webChecker() {
+        HttpRequest get = HttpUtil.createGet("http://zk.hngaozhong.com/");
+        get.timeout(5000);
+        try {
+            HttpResponse execute = get.execute();
+            int status = execute.getStatus();
+            pusher.pushTextMessage(new DingTextMessage("这个网站居然在五秒超时时间内响应了！状态码：" + status));
+        } catch (Exception e) {
+            log.info("连接超时");
+        }
+    }
 
 }
