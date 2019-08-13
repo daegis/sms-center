52d4a40d1f8ae2fc1063f5b4c39baabb2f5380a5
daegis <daegis@yeah.net>
2019-08-13 10:57:59 +0800

    update time
    update time

--- src/main/java/cn/aegisa/sms/center/schedule/StatusChecker.java
+++ src/main/java/cn/aegisa/sms/center/schedule/StatusChecker.java
@@ -24,7 +24,7 @@ public class StatusChecker {
     private TaobaoSkuService taobaoSkuService;
 
 
-    @Scheduled(cron = "0 0/20 * * * ?")
+    @Scheduled(cron = "0 0/30 * * * ?")
     public void webChecker() {
         try {
             taobaoSkuService.processTBSku();
